2384423edde922b35f156eb517dbeba8739e0fb8
Aleksey Khorkov <lsubarin@gmail.com>
2024-02-13 11:33:57 +0300

    add sem_1

---
+++ sem_1/task1/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задание 1. Совместная работа
+Семинар 1. Знакомство с языками программирования
+Напишите программу, которая на вход
+принимает два целых числа и проверяет,
+является ли первое число квадратом второго.
+a = 25, b = 5 => да
+a = 2, b = 10 => нет
+a = 9, b = -3 => да
+a = -3, b = 9 => нет */
+
+// a = 30 оператор присваивания
+// "==" Проверка на равенство - 5 == 5 (true), 4 == 6 (false)
+// "!=" Проверка на НЕравенство 5 == 5 (false), 4 == 6 (true)
+
+/* int firstNumber = 25;
+int secondNumber = 5;
+
+if(firstNumber == secondNumber*secondNumber)
+{
+    Console.WriteLine ("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+} */
+
+/* Console.WriteLine("Введите целое число : ");
+int test = Convert.ToInt32(Console.ReadLine());
+Console.Write(test); */
+
+double a = 5;
+double b = 2;
+double res = a / b;
+Console.WriteLine("Результат деления " + a + " на "+ b + " = " + res); // Конкатенация - склеивание строк
+
+
+
+
