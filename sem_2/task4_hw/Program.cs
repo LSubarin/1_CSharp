a90416b147ac0787528967a0151a41d5ed71b887
Aleksey Khorkov <lsubarin@gmail.com>
2024-02-13 13:18:47 +0300

    add sem_2:Task4_hw correct solution

--- sem_2/task4_hw/Program.cs
+++ sem_2/task4_hw/Program.cs
@@ -4,10 +4,38 @@
 показывает его цифры через запятую.
 */
 
-int number = 923879874;
-while (number / 10 > 0)
+// int number = 923879874;
+// while (number / 10 > 0)
+// {
+//     int nextDigit = number % 10;
+//     number = number / 10;
+//     Console.Write(nextDigit + ",");
+// }
+class Program
 {
-    int nextDigit = number % 10;
-    number = number / 10;
-    Console.Write(nextDigit + ",");
+    static void Main()
+    {
+        Console.Write("Введите натуральное число N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        if (N < 10)
+        {
+            Console.WriteLine(N);
+        }
+        else
+        {
+            while (N > 0)
+            {
+                int currentDigit = N % 10;
+                N /= 10;
+                if (N > 0)
+                {
+                    Console.Write(currentDigit + ",");
+                }
+                else
+                {
+                    Console.WriteLine(currentDigit);
+                }
+            }
+        }
+    }
 }
