7ec98c3caf008f007758307fb4735a24f012e714
Aleksey Khorkov <lsubarin@gmail.com>
2024-02-13 11:32:41 +0300

    add Lesson1

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Console.WriteLine("Hello world!"); //Выводим приветствие
+
+int value = 100;                    //Объявляем переменную value и присваиваем ей значение 100
+Console.WriteLine(value);           //Выводим переменную value
+
+double d = 5.5;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);checked 
---
+++ Lesson1/Task3_hw/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = 10;
+double b = 15;
+int c = 7;
+int d = 5;
+
+double result = (a * b) / (c + d);
+Console.WriteLine (result);
