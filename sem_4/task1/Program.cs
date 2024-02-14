d61c5ab1d382eca396a5087dd226f77dfca21b99
Aleksey Khorkov <lsubarin@gmail.com>
2024-02-14 13:52:55 +0300

    add sem_4:Task1 Method Calculate

--- sem_4/task1/Program.cs
+++ sem_4/task1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Тип метода ИмяМетода (параметр1,параметр2, ... ,параметр n)
+// a, b - 2 числа, sing - знак арифм. операции
+int Calculate(int a, int b, string sing)
+{
+    if (sing == "+")
+    {
+        return a + b; // сумма чисел
+    }
+    else if (sing == "-")
+    {
+        return a - b; // Разность чисел
+    }
+    else if (sing == "*")
+    {
+        return a * b; // Произведение чисел
+    }
+    else
+    {
+        Console.WriteLine("Введен неизвестный знак арифм. операции");
+        return 0;
+    }
+
+}
+
+// Вызов метода
+Console.WriteLine(Calculate(2, 3, "+"));
+Console.WriteLine(Calculate(2, 3, "-"));
+Console.WriteLine(Calculate(20, 3, "*"));
+Console.WriteLine(Calculate(2, 3, "."));
